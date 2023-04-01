ee2013f904ce6c194842f15314e21ba87ca89004
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-04-01 22:07:03 +0300

    add data for navbar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,43 @@
 import './Navbar.scss'
-
+import {Link} from 'react-router-dom'
 import React from 'react'
+import{ FaLaptopCode} from 'react-icons/fa'
 
+const data =[
+  {
+    label : 'HOME',
+    to : '/'
+  },
+  {
+    label : 'ABOUT ME',
+    to : '/about'
+  },
+  {
+    label : 'SKILLS',
+    to : '/skills'
+  },
+  {
+    label : 'RESUME',
+    to : '/resume'
+  },
+  {
+    label : 'PORTFOLIO',
+    to : '/portfolio'
+  },{
+    label : 'CONATACT',
+    to : '/contact'
+  },
+  
+]
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link>
+          <FaLaptopCode size={30} style={{color:'#fff'}}/>
+        </Link>
+      </div>
+    </nav>
   )
 }
 
