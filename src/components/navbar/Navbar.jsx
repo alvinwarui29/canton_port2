94e259b53f187319fa4d5b8927df9f04641a0580
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-04-01 22:27:06 +0300

    finish navbar design wiwthout style

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,56 +1,60 @@
-import './Navbar.scss'
-import {Link} from 'react-router-dom'
-import React from 'react'
-import{ FaLaptopCode} from 'react-icons/fa'
-
-const data =[
+import "./Navbar.scss";
+import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { FaLaptopCode, FaBars } from "react-icons/fa";
+import { HiX } from "react-icons/hi";
+const data = [
   {
-    label : 'HOME',
-    to : '/'
+    label: "HOME",
+    to: "/",
   },
   {
-    label : 'ABOUT ME',
-    to : '/about'
+    label: "ABOUT ME",
+    to: "/about",
   },
   {
-    label : 'SKILLS',
-    to : '/skills'
+    label: "SKILLS",
+    to: "/skills",
   },
   {
-    label : 'RESUME',
-    to : '/resume'
+    label: "RESUME",
+    to: "/resume",
   },
   {
-    label : 'PORTFOLIO',
-    to : '/portfolio'
-  },{
-    label : 'CONATACT',
-    to : '/contact'
+    label: "PORTFOLIO",
+    to: "/portfolio",
   },
-  
-]
+  {
+    label: "CONATACT",
+    to: "/contact",
+  },
+];
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+  const handleClick = () => {
+    setClick(!click);
+  };
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <Link to={'/'} className='navbar_logo' >
-          <FaLaptopCode size={30} style={{color:'#fff'}}/>
+        <Link to={"/"} className="navbar_logo">
+          <FaLaptopCode size={30} style={{ color: "#fff" }} />
         </Link>
       </div>
       <ul className="navbar_menu">
-        {
-          data.map((item,key)=>(
-              <li key={key} className="navbar_menu_item">
-                <Link to={item.to} className="navbar_menu_item_links">
-                    {item.label}
-                </Link>
-              </li>
-          ))
-        }
+        {data.map((item, key) => (
+          <li key={key} className="navbar_menu_item">
+            <Link to={item.to} className="navbar_menu_item_links">
+              {item.label}
+            </Link>
+          </li>
+        ))}
       </ul>
-
+      <div className="nav-icons" onClick={handleClick}>
+        {click ? <HiX size={30} style={{ color: "#fff" }} /> : <FaBars size={30} style={{ color: "#fff" }} />}
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
