a0953ee82f4bbd1d101cef14c9dccf2076e9e000
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-04-01 21:30:41 +0300

    add global styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {Routes,Route} from 'react-router-dom';
+import Home from './containers/home/Home';
+import About from './containers/about/About';
+import Portfolio from './containers/portfolio/Portfolio';
+import Skills from './containers/skills/Skills';
+import Resume from './containers/resume/Resume';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route index path='/' element={<Home/>}/>
+        <Route index path='/about' element={<About/>}/>
+        <Route index path='/portfolio' element={<Portfolio/>}/>
+        <Route index path='/skills' element={<Skills/>}/>
+        <Route index path='/resume' element={<Resume/>}/>
+
+      </Routes>
     </div>
   );
 }
