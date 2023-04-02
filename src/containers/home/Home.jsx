12c11ec2e04f226d6237c9a1fd6ef283b84d4421
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-04-02 03:29:19 +0300

    add image and transition

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -1,10 +1,15 @@
 import "./Home.scss";
 
 import React from "react";
-
+import me from "../../assets/me.jpg"
 const Home = () => {
   return (
+    
+
     <section className="home" id="home">
+    <div className="home__left">
+      <img src={me} alt="" />
+    </div>
       <div className="home__textwrapper">
         <h1>
           Hello, I'm warui
