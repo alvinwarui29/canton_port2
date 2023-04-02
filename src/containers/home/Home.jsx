968b012512809142f1d04b753c7169541e247edf
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-04-02 04:19:20 +0300

    design button

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -1,27 +1,30 @@
 import "./Home.scss";
-
+import { useNavigate } from "react-router-dom";
 import React from "react";
-import canton from "../../assets/canton.jpg"
+import canton from "../../assets/canton.jpg";
 const Home = () => {
-  return (
-    
+  const navigate = useNavigate();
+  const handleNavigate = () => {
+    navigate('/contact');
+  };
 
+  return (
     <section className="home" id="home">
-    {/* will add my image here */}
-    {/* <div className="home__left">
+      {/* will add my image here */}
+      {/* <div className="home__left">
       <img src={canton} alt="" />
     </div> */}
       <div className="home__textwrapper">
         <h1>
           Hello, I'm warui
           <br />
-          Front end developer and 
+          Front end developer and
           <br />
           database enthusiast
         </h1>
       </div>
       <div className="home__contactme">
-        <button>Contact me</button>
+        <button onClick={handleNavigate}>Contact me</button>
       </div>
     </section>
   );
