5824569f4d1354678fec7deb88908dc80672bb43
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-04-03 04:41:58 +0300

    add array of personal info

--- src/containers/about/About.jsx
+++ src/containers/about/About.jsx
@@ -3,32 +3,55 @@ import { BsFillInfoCircleFill } from "react-icons/bs";
 import React from "react";
 import "./About.scss";
 import { Animate } from "react-simple-animate";
+
+const personalInfo = [
+  {
+    label: "Name",
+    value: "Alvin Warui",
+  },
+  {
+    label: "Age",
+    value: "22",
+  },
+  {
+    label: "Country",
+    value: "Kenys",
+  },
+  {
+    label: "Email",
+    value: "alvincanton29@gmail.com",
+  },
+  {
+    label: "Phone number",
+    value: "+254743170028",
+  },
+];
+
 const About = () => {
   return (
     <section className="about" id="about">
-      <Header headerText="About" 
-      icon={<BsFillInfoCircleFill size={40} />} />
-    <Animate
-        play
-        duration={2}
-        delay={1}
-        start={{
-          transform: "translateX(-1300px)",
-        }}
-        end={{
-          transform: "translatex(0px)",
-        }}
-      >
+      <Header headerText="About" icon={<BsFillInfoCircleFill size={40} />} />
       <div className="about__content">
-        <h3>Front end developer</h3>
-        <p>
-          I'm able to offer top-notch, creative solutions to clients in a
-          variety of industries thanks to my keen attention to detail and
-          comprehensive understanding of front-end development and database
-          management.
-        </p>
+        <Animate
+          play
+          duration={2}
+          delay={1}
+          start={{
+            transform: "translateX(-1300px)",
+          }}
+          end={{
+            transform: "translatex(0px)",
+          }}
+        >
+          <h3>Front end developer</h3>
+          <p>
+            I'm able to offer top-notch, creative solutions to clients in a
+            variety of industries thanks to my keen attention to detail and
+            comprehensive understanding of front-end development and database
+            management.
+          </p>
+        </Animate>
       </div>
-      </Animate>
     </section>
   );
 };
