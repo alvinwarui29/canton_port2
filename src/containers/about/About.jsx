da9b6ad82ae6ec61fb9dcbd35e0c50c49c9e9d11
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-04-03 04:01:56 +0300

    pass in portfolio

--- src/containers/about/About.jsx
+++ src/containers/about/About.jsx
@@ -1,13 +1,12 @@
 import Header from "../../components/pageHeader/Header";
-import "./About.scss";
 import { BsFillInfoCircleFill } from "react-icons/bs";
 import React from "react";
+import "./About.scss";
 
 const About = () => {
   return (
     <div className="about" id="about">
-      <Header headerText="About" 
-      icon={<BsFillInfoCircleFill  size={40}/>} />
+      <Header headerText="About" icon={<BsFillInfoCircleFill size={40} />} />
     </div>
   );
 };
--- src/containers/portfolio/Portfolio.jsx
+++ src/containers/portfolio/Portfolio.jsx
@@ -1,10 +1,14 @@
 import "./Porfolio.scss"
-
+import Header from "../../components/pageHeader/Header";
+import { BsFillInfoCircleFill } from "react-icons/bs";
 import React from 'react'
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
+    <div className="portfolio" id="portfolio">
+      <Header headerText="portfolio" 
+      icon={<BsFillInfoCircleFill  size={40}/>} />
+    </div>
   )
 }
 
