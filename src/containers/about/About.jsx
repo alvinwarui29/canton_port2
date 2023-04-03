d5c809e464f48c9d03b3b58c859329350c59721b
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-04-03 05:06:10 +0300

    add styling for the left side

--- src/containers/about/About.jsx
+++ src/containers/about/About.jsx
@@ -15,7 +15,7 @@ const personalInfo = [
   },
   {
     label: "Country",
-    value: "Kenys",
+    value: "Kenya",
   },
   {
     label: "Email",
@@ -32,47 +32,50 @@ const About = () => {
     <section className="about" id="about">
       <Header headerText="About" icon={<BsFillInfoCircleFill size={40} />} />
       <div className="about__content">
-        <Animate
-          play
-          duration={2}
-          delay={1}
-          start={{
-            transform: "translateX(-1300px)",
-          }}
-          end={{
-            transform: "translatex(0px)",
-          }}
-        >
-          <h3>Front end developer</h3>
-          <p>
-            I'm able to offer top-notch, creative solutions to clients in a
-            variety of industries thanks to my keen attention to detail and
-            comprehensive understanding of front-end development and database
-            management.
-          </p>
-        </Animate>
-        <Animate
-          play
-          duration={2}
-          delay={1}
-          start={{
-            transform: "translateX(500px)",
-          }}
-          end={{
-            transform: "translatex(0px)",
-          }}
-        >
-        <h3>Personal info</h3>
-          <ul>
-            {
-              personalInfo.map((item,i)=>(
-              <li key={i}>
-                <span className="title">{item.label} </span>
-                <span className="value">{item.value} </span>
-              </li>
-              ))}
-          </ul>
+        <div className="about__content__personal">
+          <Animate
+            play
+            duration={2}
+            delay={1}
+            start={{
+              transform: "translateX(-1300px)",
+            }}
+            end={{
+              transform: "translatex(0px)",
+            }}
+          >
+            <h3>Front end developer</h3>
+            <p>
+              I'm able to offer top-notch, creative solutions to clients in a
+              variety of industries thanks to my keen attention to detail and
+              comprehensive understanding of front-end development and database
+              management.
+            </p>
           </Animate>
+          <Animate
+            play
+            duration={2}
+            delay={1}
+            start={{
+              transform: "translateX(500px)",
+            }}
+            end={{
+              transform: "translatex(0px)",
+            }}
+          >
+            <h3 className="personalInfomation">Personal info</h3>
+            <ul>
+              {personalInfo.map((item, i) => (
+                <li key={i}>
+                  <span className="title">{item.label} </span>
+                  <span className="value">{item.value} </span>
+                </li>
+              ))}
+            </ul>
+          </Animate>
+        </div>
+        <div className="about__content__services">
+        Services</div>
       </div>
     </section>
   );
