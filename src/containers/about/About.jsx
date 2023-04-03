8715518bd11ba139d9bec5a872bddd0a639d2dcd
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-04-03 03:59:49 +0300

    pass data from header dynamically to about

--- src/containers/about/About.jsx
+++ src/containers/about/About.jsx
@@ -1,11 +1,15 @@
-import './About.scss';
-
-import React from 'react'
+import Header from "../../components/pageHeader/Header";
+import "./About.scss";
+import { BsFillInfoCircleFill } from "react-icons/bs";
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="about" id="about">
+      <Header headerText="About" 
+      icon={<BsFillInfoCircleFill  size={40}/>} />
+    </div>
+  );
+};
 
-export default About
+export default About;
