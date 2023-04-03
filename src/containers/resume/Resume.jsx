3c0629a23b3c2b46a24de9e129f46235634c889b
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-04-03 04:06:01 +0300

    create contact file and folder and pass data

--- src/containers/resume/Resume.jsx
+++ src/containers/resume/Resume.jsx
@@ -1,15 +1,14 @@
-import "./Resume.scss"
+import "./Resume.scss";
 import Header from "../../components/pageHeader/Header";
 import { BsFillInfoCircleFill } from "react-icons/bs";
-import React from 'react'
+import React from "react";
 
 const Resume = () => {
   return (
     <div className="resume" id="resume">
-      <Header headerText="resume" 
-      icon={<BsFillInfoCircleFill  size={40}/>} />
+      <Header headerText="resume" icon={<BsFillInfoCircleFill size={40} />} />
     </div>
-  )
-}
+  );
+};
 
-export default Resume
+export default Resume;
--- src/containers/skills/Skills.jsx
+++ src/containers/skills/Skills.jsx
@@ -1,11 +1,14 @@
-import './Skills.scss'
-
-import React from 'react'
+import "./Skills.scss";
+import Header from "../../components/pageHeader/Header";
+import { BsFillInfoCircleFill } from "react-icons/bs";
+import React from "react";
 
 const Skills = () => {
   return (
-    <div>Skills</div>
-  )
-}
+    <div className="skills" id="skills">
+      <Header headerText="skills" icon={<BsFillInfoCircleFill size={40} />} />
+    </div>
+  );
+};
 
-export default Skills
+export default Skills;
