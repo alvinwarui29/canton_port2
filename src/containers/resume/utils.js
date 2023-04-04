4fc400e9064eadaefdbb915cac0856fb6b8ef690
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-04-05 01:37:57 +0300

    add the util data

---
+++ src/containers/resume/utils.js
@@ -0,0 +1,24 @@
+export const data = {
+    experience : [
+        {
+            title : "Web Developer",
+            subtitle : "Vm randa and co advocates",
+            description : "Web developer",
+        },
+        {
+            title : "ussd Application",
+            subtitle : "Africa's talking",
+            description : "Registering people in need of aid",
+        },
+        {
+            title : "student",
+            subtitle : "Kabarak University",
+            description : "Determined student",
+        },
+        {
+            title : "Teacher",
+            subtitle : "Peer teacher",
+            description: "Share my programming knowledge with my peers"
+        }
+    ]
+}
