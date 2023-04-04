28c2c64d87a4ab33475043a6d658c4d533d7395d
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-04-05 02:05:33 +0300

    design the jsx file

--- src/containers/resume/Resume.jsx
+++ src/containers/resume/Resume.jsx
@@ -2,13 +2,43 @@ import "./Resume.scss";
 import Header from "../../components/pageHeader/Header";
 import { BsFillInfoCircleFill } from "react-icons/bs";
 import React from "react";
+import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
+import 'react-vertical-timeline-component/style.min.css'
+import { data } from './utils';
 
 const Resume = () => {
   return (
-    <div className="resume" id="resume">
+    <section className="resume" id="resume">
       <Header headerText="resume" icon={<BsFillInfoCircleFill size={40} />} />
+  
+    <div className="timeline">
+        <div className="timeline__experience">
+            <h3>My Works</h3>
+            <VerticalTimeline
+            layout={'1-column'}
+            lineColor="var(--yellow-theme-main-color)"
+            >
+            {
+              data.experience.map((item,i)=>(
+                  <VerticalTimelineElement
+                  key={i}
+                  className="timeline__experience__vertical-timeline-element"
+                  >
+                  <div>
+                    <h3>{item.title}</h3>
+                    <h4>{item.subtitle}</h4>
+                  </div>
+                  <p>{item.description}</p>
+                  </VerticalTimelineElement>
+              ))
+            }
+
+            </VerticalTimeline>
+        </div>
     </div>
-  );
+    </section>
+    
+    );
 };
 
 export default Resume;
